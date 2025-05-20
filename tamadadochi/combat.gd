63e1c768a7ac5c43dfa0923315c1695bc9e2030e
theduodecim <juansebastian.012@gmail.com>
2025-05-20 18:50:36 -0300

    fix

--- tamadadochi/combat.gd
+++ tamadadochi/combat.gd
@@ -1,10 +1,19 @@
 extends Node
 
-
+var stat_label : Label = null
 
 
 func _ready():
+	stat_label = get_node("StatPanel/statLabel")
 	print("💥 Stats loaded from previous scene:")
 	print("Fuerza:", GlobalStats.fuerza_level)
 	print("Magia:", GlobalStats.magia_level)
 	print("Defensa:", GlobalStats.defensa_level)
+
+	# Show values in the stat label
+	if stat_label:
+		stat_label.text = "💥 Stats :\nFuerza: %d\nMagia: %d\nDefensa: %d" % [
+			GlobalStats.fuerza_level,
+			GlobalStats.magia_level,
+			GlobalStats.defensa_level
+		]
